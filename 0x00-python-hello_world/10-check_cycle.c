6a116344aa888a800024c5b43e9574ded544ca02
Darius Tohtin <dariustohtin@gmail.com>
2023-11-27 12:14:12 -0800

    Created c file 10-check_cycle.c lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - finds loop in a linked list
+ *
+ * @list: pointer to first node of the list
+ *
+ * Return: pointer to node where loop starts
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
