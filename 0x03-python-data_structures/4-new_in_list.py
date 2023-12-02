f0a87288403920f0afe4a5d78f16370a5cdb6846
Darius Tohtin <dariustohtin@gmail.com>
2023-12-02 05:26:34 -0800

    Edited 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    for idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = [x for x in my_list]
+    new_list[idx] = element
+    return new_list
