e196eee309aa9897d6334e88cf61b045845099e4
Darius Tohtin <dariustohtin@gmail.com>
2023-12-06 12:54:37 -0800

    Edited 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string:
-        return None
+        return 0
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     for i in range(len(roman_string)):
