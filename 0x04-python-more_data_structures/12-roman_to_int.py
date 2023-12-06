e23101c4eecc37de064b0536b8805f17e3d44348
Darius Tohtin <dariustohtin@gmail.com>
2023-12-06 12:28:49 -0800

    Edited 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,11 @@ def roman_to_int(roman_string):
         return None
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
-    for char in roman_string:
-        if my_dict[char] > value:
-            value = my_dict[char] - value
+    for i in range(len(roman_string)):
+        current = my_dict[roman_string[i]]
+        new = my_dict[roman_string[i + 1]] if i < len(roman_string) - 1 else 0
+        if current < new:
+            value -= current
         else:
-            value += my_dict[char]
+            value += current
     return value
