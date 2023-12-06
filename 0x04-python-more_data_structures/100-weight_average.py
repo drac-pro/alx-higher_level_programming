df5e685e6ba40079692ec3eebda12595a1551967
Darius Tohtin <dariustohtin@gmail.com>
2023-12-06 13:59:35 -0800

    Edited 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not isinstance(my_list, list):
+        return 0
     numerator, denomenator = 0, 0
     for score in my_list:
         numerator += (score[0] * score[1])
