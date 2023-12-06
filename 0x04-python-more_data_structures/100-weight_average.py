e4bae118e6b81c2d363ec4ab6eb28038f2c143d1
Darius Tohtin <dariustohtin@gmail.com>
2023-12-06 13:53:09 -0800

    Created script 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    numerator, denomenator = 0, 0
+    for score in my_list:
+        numerator += (score[0] * score[1])
+        denomenator += score[1]
+    return numerator/denomenator
+
