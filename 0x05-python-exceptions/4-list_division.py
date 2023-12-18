8008f0280feeab84a16ff5bea405832281d03a59
Darius Tohtin <dariustohtin@gmail.com>
2023-12-18 05:54:58 -0800

    Created script 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (TypeError, ValueError):
+        except TypeError:
             print("wrong type")
             result = 0
         except IndexError:
