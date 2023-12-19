bfa3966933c8f956033637b22b5ecdb7e7d95f53
Darius Tohtin <dariustohtin@gmail.com>
2023-12-19 10:18:18 -0800

    Created script 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,7 +23,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size == value
+        self.__size = value
 
     def area(self):
         """
