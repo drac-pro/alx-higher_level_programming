236e958d4969a903e23674a2afe98f9e35addeb3
Darius Tohtin <dariustohtin@gmail.com>
2023-12-19 11:41:53 -0800

    Created script 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,10 +48,10 @@ class Square:
     def my_print(self):
         """prints the square with the character #"""
         if self.__size == 0:
-            print()
+            print('')
             return
-        [print() for i in range(self.__position[1])]
+        [print('') for i in range(self.__position[1])]
         for i in range(self.__size):
             print(" "*self.__position[0], end='')
             print('#'*self.__size, end='')
-            print()
+            print('')
