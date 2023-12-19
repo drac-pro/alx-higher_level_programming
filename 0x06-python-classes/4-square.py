0f1f5fb7ef8e14ad3f840a659261060b16ddcdbe
Darius Tohtin <dariustohtin@gmail.com>
2023-12-19 10:04:01 -0800

    Created script 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Creates a class Square"""
+
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """ Initializes a new Square
+
+        args:
+            size(int): size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """setter/getter of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size == value
+
+    def area(self):
+        """
+        Returns:
+            the area of the square
+        """
+        return self.__size**2
