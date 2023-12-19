92e3a66d6b3bce69296989f01a5dbfa90a8775ae
Darius Tohtin <dariustohtin@gmail.com>
2023-12-19 09:14:14 -0800

    Created script 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a class Square"""
+
+
+class Square:
+    """Represent a square"""
+    def __init__(self, size):
+        """ Initializes a new Square
+
+        args:
+            size(int): size of the square
+        """
+        self.__size = size
