a3d29efc311bb27bc3855ea382d4685d39904122
Darius Tohtin <dariustohtin@gmail.com>
2024-01-08 08:42:48 -0800

    Created 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """An empty class BaseGeometry"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
