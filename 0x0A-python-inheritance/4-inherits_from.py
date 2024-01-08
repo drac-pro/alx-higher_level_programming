46ce90073c348bca619641cf81ee5e9b3357c526
Darius Tohtin <dariustohtin@gmail.com>
2024-01-08 08:28:09 -0800

    Created 4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """checks if the object is an instance of a class that inherited (directly
+    or indirectly) from the specified class
+    Args:
+        obj (object): an object instance
+        a_class (type): a class
+    Returns:
+        bool: True or False
+    """
+    return True if issubclass(type(obj), a_class)  and type(obj) != a_class else False
