8a1249f7fc3330fbc724f05e084cf45fcaff2abe
Darius Tohtin <dariustohtin@gmail.com>
2024-01-08 06:35:46 -0800

    Created 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a lookup function"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object
+    Args:
+        obj (object): an object
+    Returns:
+        list: list of attributes and methods of the object
+    """
+    if isinstance(obj, object):
+        return list(obj.__dict__)
