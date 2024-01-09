0dc6087b4716ffe4270dd1eaaa832cf3ba64e2a8
Darius Tohtin <dariustohtin@gmail.com>
2024-01-09 11:36:46 -0800

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines class Student"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student object
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance"""
+        if type(attrs) == list and all(type(ele) == str for ele in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
