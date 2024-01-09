3ab5c9fe9a5700f5d1f02bf7f520eec8f96feaa5
Darius Tohtin <dariustohtin@gmail.com>
2024-01-09 12:47:42 -0800

    Created 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines pascal_triangle function"""
+
+
+def pascal_triangle(n):
+    """"returns a list of lists of integers representing the Pascal’s
+    triangle of n
+    Args:
+        n (int): the pascal triangle number
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == 1:
+                coef = 1
+            else:
+                coef = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row.append(coef)
+        triangle.append(row)
