a8adb31446c8b67cbdb1524b75972661a84477cd
Darius Tohtin <dariustohtin@gmail.com>
2024-01-09 12:51:47 -0800

    Created 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,11 +13,11 @@ def pascal_triangle(n):
     triangle = []
     for i in range(n):
         row = []
-        for j in range(i + 1):
-            if j == 0 or j == 1:
+        for j in range(i+1):
+            if j == 0 or j == i:
                 coef = 1
             else:
-                coef = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                coef = triangle[i-1][j-1] + triangle[i-1][j]
             row.append(coef)
         triangle.append(row)
     return triangle
