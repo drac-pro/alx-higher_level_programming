972f3d670f1aa4c6c416c817601326c0c33c58d8
Darius Tohtin <dariustohtin@gmail.com>
2024-01-09 10:50:58 -0800

    Created 9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines class Student"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student object
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of a Student instance"""
+        return self.__dict__
