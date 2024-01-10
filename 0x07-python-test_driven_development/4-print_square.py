4e3c597893b2746f21903181f5e582be6fcd8167
Darius Tohtin <dariustohtin@gmail.com>
2024-01-10 13:55:38 -0800

    Edited

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,6 +12,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     if size == 0:
-        print()
+        pass
     else:
         [print('#'*size) for i in range(size)]
