ee9e8ae571d9bb9509fba1e753060fb88949ed9e
Darius Tohtin <dariustohtin@gmail.com>
2024-03-11 07:44:55 -0700

    Edited 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length === 3) {
-  console.log('Argument found');
-} else if (process.argv.length > 3) {
-  console.log('Arguments found');
-} else {
+if (process.argv.length === 2) {
   console.log('No Argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
