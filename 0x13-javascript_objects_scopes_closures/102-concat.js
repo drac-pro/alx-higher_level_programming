46d00c58366bf104d120ae1c41c3efcdbff073ae
Darius Tohtin <dariustohtin@gmail.com>
2024-03-12 14:34:14 -0700

    Created 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const path = require('path');
+if (process.argv.lenght >= 5) {
+  const [,, firstFilePath, secondFilePath, destinationFilePath] = process.argv;
+  const firstFileContent = fs.readFileSync(path.resolve(firstFilePath), { encoding: 'utf8' });
+  const secondFileContent = fs.readFileSync(path.resolve(secondFilePath), { encoding: 'utf8' });
+  const concatenatedContent = firstFileContent + secondFileContent;
+  fs.writeFileSync(path.resolve(destinationFilePath), concatenatedContent);
+}
