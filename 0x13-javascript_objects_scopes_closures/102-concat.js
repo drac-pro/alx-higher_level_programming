5557de4a3ebf632e7b9f5345ec3411ea02b93f82
Darius Tohtin <dariustohtin@gmail.com>
2024-03-12 14:38:26 -0700

    Edited 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const path = require('path');
-if (process.argv.lenght >= 5) {
+if (process.argv.length >= 5) {
   const [,, firstFilePath, secondFilePath, destinationFilePath] = process.argv;
   const firstFileContent = fs.readFileSync(path.resolve(firstFilePath), { encoding: 'utf8' });
   const secondFileContent = fs.readFileSync(path.resolve(secondFilePath), { encoding: 'utf8' });
