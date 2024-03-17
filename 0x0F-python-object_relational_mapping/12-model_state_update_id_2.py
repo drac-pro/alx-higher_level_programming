a864735535df4988c42439694842a4d5fb2fead8
Darius Tohtin <dariustohtin@gmail.com>
2024-03-17 15:30:54 -0700

    Created 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""changes the name of a State object from the database hbtn_0e_6_usa
+Change the name of the State where id = 2 to New Mexico
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                           argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state_instance = session.query(State).filter_by(id=2).one()
+    state_instance.name = 'New Mexico'
+    session.commit()
+    session.close()
