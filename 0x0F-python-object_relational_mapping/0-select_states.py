a7932eacb68145cb83ab232730786fd2ae3cab0c
Darius Tohtin <dariustohtin@gmail.com>
2024-03-17 05:58:09 -0700

    Created 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3], charset="utf8")
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
