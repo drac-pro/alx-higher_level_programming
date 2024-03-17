5dbd5098a74fd69cdfc1d30daa877d8849949dc4
Darius Tohtin <dariustohtin@gmail.com>
2024-03-17 13:17:42 -0700

    Created model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""contains the class definition of a State and an instance Base = declarative_base()"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """defines a State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
