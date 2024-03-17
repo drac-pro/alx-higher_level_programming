d18695018d57646954daae36820558df7274605c
Darius Tohtin <dariustohtin@gmail.com>
2024-03-17 13:19:13 -0700

    Created model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance Base = declarative_base()"""
+"""contains the class definition of a State and an instance
+Base = declarative_base()"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
