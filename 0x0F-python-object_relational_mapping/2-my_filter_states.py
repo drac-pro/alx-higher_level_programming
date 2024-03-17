6a6024b7e3cb8fba6969a9a269c474f22f09ca9e
Darius Tohtin <dariustohtin@gmail.com>
2024-03-17 07:17:30 -0700

    Created 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument."""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close
+    conn.close
