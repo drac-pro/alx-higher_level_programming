a1facf8404336857fa6680c04141595c9d9f0ffe
Darius Tohtin <dariustohtin@gmail.com>
2024-03-27 10:03:51 -0700

    Created 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | grep "Status" | cut -d " " -f2-
+curl -o /dev/null -s -w "%{http_code}" "$1"
